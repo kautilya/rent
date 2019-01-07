3021dd6b340d987215ea951c9d0607e85e38c3d2
Shirish Rai <shirish.rai@kautilya.org>
2019-01-06 20:21:39 -0800

    fix left over changes.

--- mortgage.py
+++ mortgage.py
@@ -28,7 +28,7 @@ class Rent:
         self._expense = dollar(0.0)
         self._hoa = dollar(0.0)
         self._rent_increase_percent = decimal.Decimal(0.0)
-        self._idle_months = 0.0
+        self._idle_months = decimal.Decimal(0.0)
         self._next_rent = dollar(0.0)
         self._next_insurance = dollar(0.0)
         self._next_expense = dollar(0.0)
@@ -37,13 +37,13 @@ class Rent:
     def setup(self, params):
         if (params._rent != 0 and params._rent_per_sqft != 0):
             raise ValueError("Cannot have both rent and rent-per-sq-feet")
-        if ((params._rent_per_sqft != 0 and params._sqft !=0) or (params._sqft != 0 and params._rent_per_sqft == 0)):
+        if ((params._rent_per_sqft == 0 and params._sqft !=0) or (params._sqft != 0 and params._rent_per_sqft == 0)):
             raise ValueError("sqft and rent-per-sq-feet must be both 0 or set to some value")
         if params._rent != 0.0 : 
             self._rent = dollar(params._rent)
         else:
             self._rent = dollar(params._sqft * params._rent_per_sqft)
-        self.next_rent = self._rent
+        self._next_rent = self._rent
         self._insurance = dollar(params._insurance)
         self._next_insurance = self._insurance
         self._expense = dollar(params._expense)
@@ -51,7 +51,7 @@ class Rent:
         self._hoa = dollar(params._hoa)
         self._next_hoa = self._hoa
         self._rent_increase_percent = decimal.Decimal(params._rent_increase_percent)
-        self._idle_months = params._idle_months
+        self._idle_months = decimal.Decimal(params._idle_months)
 
     def rent(self):
         return self._rent
@@ -69,7 +69,7 @@ class Rent:
         return self._rent_increase_percent
 
     def idle_months(self):
-        return self.idle_months
+        return self._idle_months
 
     def update_next(self):
         self._next_rent = inflate(self._next_rent, self._rent_increase_percent)
@@ -90,40 +90,40 @@ class Rent:
         return self._next_hoa
 
     def annual_rent(self):
-        return self.next_rent() * (float(MONTHS_IN_YEAR) - self._idle_months)
+        return self.next_rent() * (decimal.Decimal(MONTHS_IN_YEAR) - self._idle_months)
 
 class Mortgage:
     def __init__(self):
-        self._interest = 0.0
+        self._interest = decimal.Decimal(0.0)
         self._months = 0
         self._loan = dollar(0.0)
         self._value = dollar(0.0)
         self._down = dollar(0.0)
-        self._taxrate = 0.0
+        self._taxrate = decimal.Decimal(0.0)
         self._tax_increase_percent = decimal.Decimal(0)
         self._appreciation_percent = decimal.Decimal(0)
-        self._tax = 0.0
-        self._next_tax = 0.0
+        self._tax = decimal.Decimal(0.0)
+        self._next_tax = decimal.Decimal(0.0)
         self._next_down = self._down
         self._next_value = self._value
-        self._equity = 0.0
+        self._equity = decimal.Decimal(0.0)
 
     def setup(self, params):
-        self._interest = float(params._interest) / 100
-        self._months = int(params._months)
+        self._interest = decimal.Decimal(decimal.Decimal(params._interest) / decimal.Decimal(100.0))
+        self._months = decimal.Decimal(params._months)
         self._value = dollar(params._value)
         if (params._down != 0):
             self._down = dollar(params._down)
         else:
-            self._down = dollar(self._value * params._down_fraction)
+            self._down = dollar(self._value * decimal.Decimal(params._down_fraction))
         self._loan = dollar(self._value - self._down)
         self._equity = self._down
-        self._taxrate = float(params._taxrate)
+        self._taxrate = decimal.Decimal(params._taxrate)
         self._tax_increase_percent = decimal.Decimal(params._tax_increase_percent)
         self._appreciation_percent = decimal.Decimal(params._appreciation_percent)
         if (self._tax_increase_percent > self._appreciation_percent):
             self._tax_increase_percent = self._appreciation_percent
-        self._tax = self._value * (self._taxrate / 100)
+        self._tax = self._value * (self._taxrate / decimal.Decimal(100.0))
         self._next_tax = self._tax
         self._next_down = self._down
         self._next_value = self._value
@@ -132,7 +132,7 @@ class Mortgage:
         return self._interest
 
     def month_growth(self):
-        return 1. + self._interest / MONTHS_IN_YEAR
+        return decimal.Decimal(1.) + self._interest / decimal.Decimal(MONTHS_IN_YEAR)
 
     def apy(self):
         return self.month_growth() ** MONTHS_IN_YEAR - 1
@@ -161,7 +161,7 @@ class Mortgage:
     def down(self):
         return self._down
 
-    def next_down(self, principle):
+    def next_down(self):
         return self._next_down
 
     def equity(self):
@@ -173,11 +173,14 @@ class Mortgage:
     def tax_increase_percent(self):
         return self._tax_increase_percent
 
+    def appreciation_percent(self):
+        return self._appreciation_percent
+
     def next_tax(self):
         return self._next_tax
 
     def monthly_payment(self):
-        pre_amt = float(self.loan()) * self.rate() / (float(MONTHS_IN_YEAR) * (1.-(1./self.month_growth()) ** self.loan_months()))
+        pre_amt = decimal.Decimal(self.loan()) * decimal.Decimal(self.rate()) / (decimal.Decimal(MONTHS_IN_YEAR) * decimal.Decimal((decimal.Decimal(1.)-(decimal.Decimal(1.)/self.month_growth()) ** self.loan_months())))
         return dollar(pre_amt, round=decimal.ROUND_CEILING)
 
     def total_value(self, m_payment):
@@ -198,7 +201,7 @@ class Mortgage:
             interest = dollar(interest_unrounded, round=decimal.ROUND_HALF_UP)
             if monthly >= balance + interest:
                 principle = balance
-                balance = 0.0
+                balance = decimal.Decimal(0.0)
                 yield balance, principle, interest
                 break
             principle = monthly - interest
@@ -208,31 +211,31 @@ class Mortgage:
 class Config:
     
     def __init__(self):
-        _interest = 0.0
-        _months = 0.0
-        _value = 0.0
-        _down = 0.0
-        _down_fraction = 0.0
-        _rent = 0.0
-        _rent_per_sqft = 0.0
-        _insurance = 0.0
-        _taxrate = 0.0
-        _hoa = 0.0
-        _expense = 0.0
-        _rent_increaase_percent = 0.0
-        _tax_increase_percent = 0.0
-        _idle_months = 0.0
-        _sqft = 0.0
-        _appreciation_percent = 0.0
+        self._interest = 0.0
+        self._months = 0.0
+        self._value = 0.0
+        self._down = 0.0
+        self._down_fraction = 0.0
+        self._rent = 0.0
+        self._rent_per_sqft = 0.0
+        self._insurance = 0.0
+        self._taxrate = 0.0
+        self._hoa = 0.0
+        self._expense = 0.0
+        self._rent_increaase_percent = 0.0
+        self._tax_increase_percent = 0.0
+        self._idle_months = 0.0
+        self._sqft = 0.0
+        self._appreciation_percent = 0.0
 
     def check(self):
-        if _interest <= 0.0:
+        if self._interest <= 0.0:
             raise ValueError("Invalid Interest")
-        if _months <= 0.0
+        if self._months <= 0.0:
             raise ValueError("Invalid Months")
-        if _down <= 0.0 and _down_fraction <= 0.0:
+        if self._down <= 0.0 and self._down_fraction <= 0.0:
             raise ValueError("Invalid down or down-fraction")
-        if _rent <= 0 and _rent_per_sqft <= 0:
+        if self._rent <= 0 and self._rent_per_sqft <= 0:
             raise ValueError("Invalid rent or rent-per-sq-feet")
 
     @staticmethod
@@ -265,7 +268,7 @@ class Config:
             else:
                 if 'down-fraction' in config:
                     current._down_fraction = config['down-fraction']
-                else if 'down' in config:
+                elif 'down' in config:
                     current._down = config['down']
 
         if 'rent-per-sq-feet' in config and 'rent' in config:
@@ -277,7 +280,7 @@ class Config:
             else:
                 if 'rent-per-sq-feet' in config:
                     current._rent_per_sqft = config['rent-per-sq-feet']
-                else if 'rent' in config:
+                elif 'rent' in config:
                     current._rent = config['rent']
 
         if 'value' in config:
@@ -303,7 +306,7 @@ class Config:
         if 'rent-increase-percent' in config:
             current._rent_increase_percent = config['rent-increase-percent']
         else:
-            current._rent_increase_percent = previous._rent_increase_precent
+            current._rent_increase_percent = previous._rent_increase_percent
 
         if 'tax-increase-percent' in config:
             current._tax_increase_percent = config['tax-increase-percent']
@@ -316,7 +319,7 @@ class Config:
             current._idle_months = previous._idle_months
 
         if 'appreciation-percent' in config:
-            current._appreciation_parcent = config['appreciation-percent']
+            current._appreciation_percent = config['appreciation-percent']
         else:
             current._appreciation_percent = previous._appreciation_percent
 
@@ -326,6 +329,14 @@ class Config:
             current._sqft = previous._sqft
 
         return current
+
+def print_parameters(m, r):
+    print('{0:>12s} {1:>12s} {2:>12s} {3:>12s} {4:>12s} {5:>12s} {6:>12s}'.format('Interest', 'months', 'loan', 'value', 'down', 'tax', 'tax inc'))
+    print('{0:>12.2f} {1:>12.0f} {2:>12.2f} {3:>12.2f} {4:>12.2f} {5:>12.2f} {6:>12.0f}'.\
+            format(m.rate() * decimal.Decimal(100), m.loan_months(), m.loan(), m.value(), m.down(), m.taxrate(), m.tax_increase_percent()))
+    print('{0:>12s} {1:>12s} {2:>12s} {3:>12s} {4:>12s} {5:>12s} {6:>12s}'.format('value inc', 'rent', 'insurance', 'expense', 'hoa', 'rent inc', 'idle months'))
+    print('{0:>12.0f} {1:>12.2f} {2:>12.2f} {3:>12.2f} {4:>12.2f} {5:>12.0f} {6:>12.2f}'.\
+            format(m.appreciation_percent(), r.rent(), r.insurance(), r.expense(), r.hoa(), r.rent_increase_percent(), r.idle_months()))
 
 def print_summary(m):
     print('{0:>25s}:  {1:>12.6f}'.format('Rate', m.rate()))
@@ -339,13 +350,14 @@ def print_summary(m):
     print('{0:>25s}:  {1:>12.2f}'.format('Total Payout', m.total_payout()))
 
 def print_detail(m, r):
-    yinterest = 0.0
-    yprinciple = 0.0
-    lequity = 0.0
+    yinterest = decimal.Decimal(0.0)
+    yprinciple = decimal.Decimal(0.0)
+    lequity = decimal.Decimal(m.down())
     cmonth = 0
     cyear = 1
+    first = False
 
-    print('{0:>25s}:'.format('Yearly Schedule'))
+    print('{0:>40s}:'.format('Yearly Schedule'))
     print('{0:>12s} {1:>12s} {2:>12s} {3:>12s} {4:>12s} {5:>12s} {6:>12s} {7:>12s} {8:>12s} {9:>12s}'.format('Year', 'Interest', 'Principle', 'Expense', 'Rent', 'Profit', 'Porf Pct', 'Cash Flow', 'ProfitWEq', 'PWEq Pct'))
     for balance, principle, interest in m.monthly_payment_schedule():
         yinterest = yinterest + interest
@@ -353,8 +365,10 @@ def print_detail(m, r):
         cmonth = (cmonth + 1) % MONTHS_IN_YEAR
 
         if cmonth == 0:
-            m.next_update(balance, yprinciple)
-            r.next_update()
+            if first :
+                first = False
+                continue
+
             expense = (m.next_tax() + (r.next_expense() * MONTHS_IN_YEAR) + 
                       (r.next_hoa() * MONTHS_IN_YEAR) 
                       + (r.next_insurance() * MONTHS_IN_YEAR) + yinterest)
@@ -370,6 +384,8 @@ def print_detail(m, r):
             yprinciple = 0
             cyear = cyear + 1
             lequity = yequity
+            m.update_next(balance, yprinciple)
+            r.update_next()
 
     expense = (m.next_tax() + (r.next_expense() * MONTHS_IN_YEAR) + 
             (r.next_hoa() * MONTHS_IN_YEAR) 
@@ -378,14 +394,17 @@ def print_detail(m, r):
     profit = rent - expense
     profit_percent = (profit / m.next_down()) * 100
     cash_flow = rent - expense - yprinciple
-    print('{0:>12d} {1:>12.2f}  {2:>12.2f} {3:>12.2f} {4:>12.2f} {5:>12.2f} {6:>12.2f} {7:>12.2f}'.format(cyear, 0, 0, expense, rent, profit, profit_percent, cash_flow));
+    yequity = m.equity()
+    profit_with_equity = profit + (yequity - lequity)
+    profit_with_equity_percent = (profit_with_equity / m.next_down()) * 100
+    print('{0:>12d} {1:>12.2f}  {2:>12.2f} {3:>12.2f} {4:>12.2f} {5:>12.2f} {6:>12.2f} {7:>12.2f} {8:>12.2f} {9:>12.2f}'.format(cyear, 0, 0, expense, rent, profit, profit_percent, cash_flow, profit_with_equity, profit_with_equity_percent));
 
 def main():
     parser = argparse.ArgumentParser(description='Mortgage Amortization Tools')
     parser.add_argument('-s', '--summary', default=False, dest='summary')
     parser.add_argument('-f', '--cfile', dest='cfile')
     parser.add_argument('-a', '--afile', default='./area.json', dest='afile')
-    parser.add_argument('-g', '--gfile', default='../global.json', dest='gfile')
+    parser.add_argument('-g', '--gfile', default='./global.json', dest='gfile')
     args = parser.parse_args()
 
     params = None
@@ -393,28 +412,34 @@ def main():
         try:
             with open(args.gfile) as global_config:
                 config = json.load(global_config)
-                params = Config.save(None)
+                params = Config.save(None, config)
         except FileNotFoundError:
+            pass
 
     if args.afile:
         try:
             with open(args.afile) as area_config:
                 config = json.load(area_config)
-                params = Config.save(params)
+                params = Config.save(params, config)
         except FileNotFoundError:
+            pass
         
     if args.cfile:
         try:
             with open(args.cfile) as config_file:
                 config = json.load(config_file)
-                params = Config.save(params)
+                params = Config.save(params, config)
         except FileNotFoundError:
+            pass
         
     params.check()
 
-    r = Rent(params)
-    m = Mortgage(params)
+    r = Rent()
+    m = Mortgage()
+    r.setup(params)
+    m.setup(params)
 
+    print_parameters(m,r)
     if args.summary:
         print_summary(m)
     else:
